9d40c396447bfcbe20dfb07bdf1e86e4a6045306
DangThuc <thucldph22733@fpt.edu.vn>
2024-05-19 22:51:43 +0700

    'login-manage'

---
+++ src/main/java/com/example/shoesmanagement/service/NhanVienService.java
@@ -0,0 +1,33 @@
+package com.example.shoesmanagement.service;
+
+import com.example.shoesmanagement.model.ChucVu;
+import com.example.shoesmanagement.model.NhanVien;
+
+import java.io.InputStream;
+import java.util.List;
+import java.util.UUID;
+
+public interface NhanVienService {
+
+    NhanVien checkByEmailAndPass(String email, String pass);
+
+    NhanVien checkBySDTAndPass(String sdt, String pass);
+
+    public List<NhanVien> getAllNhanVien();
+
+    public void save(NhanVien nhanVien);
+
+    public void deleteByIdNhanVien(UUID id);
+
+    public NhanVien getByIdNhanVien(UUID id);
+
+    public void importDataFromExcel(InputStream excelFile);
+
+
+    public List<NhanVien> findByChucVu(ChucVu chucVu);
+
+    public List<NhanVien> findByTrangThai(int trangThai);
+
+    public List<NhanVien> fillterNhanVien(String maNV, String tenNV);
+
+}
