4c916bfb8034bdd24209b36583972a8140489b92
thuybeo03 <thuynmph22235@fpt.edu.vn>
2024-05-20 03:11:16 +0700

    ahih

---
+++ src/main/java/com/example/shoesmanagement/service/HoaDonService.java
@@ -0,0 +1,26 @@
+package com.example.shoesmanagement.service;
+
+import com.example.shoesmanagement.model.HoaDon;
+import com.example.shoesmanagement.model.KhachHang;
+import com.example.shoesmanagement.model.NhanVien;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface HoaDonService {
+    List<HoaDon> getListHoaDonChuaThanhToan();
+    void add(HoaDon hoaDon);
+    HoaDon getOne(UUID id);
+    List<HoaDon> listHoaDonKhachHangAndTrangThaiOnline(KhachHang khachHang, int trangThai);
+    List<HoaDon> findHoaDonByKhachHang(KhachHang khachHang);
+    List<HoaDon>  listHoaDonOnline();
+    List<HoaDon> listHoaDonByNhanVienAndTrangThai(NhanVien nhanVien, int trangThai);
+
+    List<HoaDon> listHoaDonHuyAndThanhCongByNhanVien(NhanVien nhanVien);
+
+    List<HoaDon> listAllHoaDonByNhanVien(NhanVien nhanVien);
+
+    List<HoaDon> listAllHoaDonByNhanVienHienTai(NhanVien nhanVien);
+
+
+}
---
+++ src/main/java/com/example/shoesmanagement/service/LSThanhToanService.java
@@ -0,0 +1,7 @@
+package com.example.shoesmanagement.service;
+
+import com.example.shoesmanagement.model.LichSuThanhToan;
+
+public interface LSThanhToanService {
+    void addLSTT(LichSuThanhToan lichSuThanhToan);
+}
