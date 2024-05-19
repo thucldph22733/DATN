5511304437ec74cd0ea0fd6144deb18aad4a35a4
quanlmph23859 <117793121+quanlmph23859@users.noreply.github.com>
2024-05-20 00:50:46 +0700

    aaa

--- src/main/java/com/example/shoesmanagement/service/KhachHangService.java
+++ src/main/java/com/example/shoesmanagement/service/KhachHangService.java
@@ -2,23 +2,35 @@ package com.example.shoesmanagement.service;
 
 import com.example.shoesmanagement.model.KhachHang;
 
+import java.io.InputStream;
 import java.util.List;
 import java.util.UUID;
 
 public interface KhachHangService {
-    KhachHang checkLoginEmail(String email, String pass);
-    KhachHang checkEmail(String email);
     KhachHang checkLoginSDT(String sdt, String pass);
+
+    KhachHang checkLoginEmail(String email, String pass);
+
+    KhachHang checkEmail(String email);
+
 
     public List<KhachHang> getAllKhachHang();
 
     public void save(KhachHang khachHang);
-    public void getByIdKhachHang(UUID id);
 
-    public void addKhachHang(KhachHang khachHang);
+    public void deteleByIdKhachHang(UUID id);
+
+    public KhachHang getByIdKhachHang(UUID id);
+
+    void addKhachHang(KhachHang khachHang);
+
+    public void importDataFromExcel(InputStream excelFile);
+
     public List<KhachHang> findByTrangThai(int trangThai);
-    public List<KhachHang> fillterKhachHang(String maKh, String tenKh);
+    public List<KhachHang> fillterKhachHang(String maKH, String tenKH);
+
 
     List<KhachHang> findKhachHangByTrangThai();
+
     List<KhachHang> findKhachHangByKeyword(String keyword);
 }
--- src/main/java/com/example/shoesmanagement/service/impl/KhachHangServiceImpl.java
+++ src/main/java/com/example/shoesmanagement/service/impl/KhachHangServiceImpl.java
@@ -4,9 +4,15 @@ import com.example.shoesmanagement.model.KhachHang;
 import com.example.shoesmanagement.repository.KhachHangRepository;
 import com.example.shoesmanagement.service.GioHangService;
 import com.example.shoesmanagement.service.KhachHangService;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.InputStream;
+import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
@@ -14,15 +20,6 @@ import java.util.UUID;
 public class KhachHangServiceImpl  implements KhachHangService {
     @Autowired
     private KhachHangRepository khachHangRepository;
-    @Override
-    public KhachHang checkLoginEmail(String email, String pass) {
-        return khachHangRepository.findByEmailKHAndTrangThaiAndMatKhau(email,1,pass);
-    }
-
-    @Override
-    public KhachHang checkEmail(String email) {
-        return khachHangRepository.findByEmailKH(email);
-    }
 
     @Override
     public KhachHang checkLoginSDT(String sdt, String pass) {
@@ -30,6 +27,15 @@ public class KhachHangServiceImpl  implements KhachHangService {
     }
 
     @Override
+    public KhachHang checkLoginEmail(String email, String pass) {
+        return khachHangRepository.findByEmailKHAndTrangThaiAndMatKhau(email, 1, pass);
+    }
+
+    @Override
+    public KhachHang checkEmail(String email) {
+        return khachHangRepository.findByEmailKH(email);
+    }
+
     public List<KhachHang> getAllKhachHang() {
         return khachHangRepository.findAll();
     }
@@ -40,8 +46,14 @@ public class KhachHangServiceImpl  implements KhachHangService {
     }
 
     @Override
-    public void getByIdKhachHang(UUID id) {
-        khachHangRepository.findById(id).orElse(null);    }
+    public void deteleByIdKhachHang(UUID id) {
+        khachHangRepository.deleteById(id);
+    }
+
+    @Override
+    public KhachHang getByIdKhachHang(UUID id) {
+        return khachHangRepository.findById(id).orElse(null);
+    }
 
     @Override
     public void addKhachHang(KhachHang khachHang) {
@@ -49,16 +61,46 @@ public class KhachHangServiceImpl  implements KhachHangService {
     }
 
     @Override
+    public void importDataFromExcel(InputStream excelFile) {
+        try (Workbook workbook = new XSSFWorkbook(excelFile)) {
+            Sheet sheet = workbook.getSheetAt(0); // Lấy sheet đầu tiên (index 0)
+
+            for (Row row : sheet) {
+                if (row.getRowNum() == 0) {
+                    // Bỏ qua hàng đầu tiên nếu nó là tiêu đề
+                    continue;
+                }
+                KhachHang khachHang = new KhachHang();
+                khachHang.setMaKH(row.getCell(0).getStringCellValue()); // Cột 0 trong tệp Excel
+                khachHang.setHoTenKH(row.getCell(1).getStringCellValue()); // Cột 1 trong tệp Excel
+
+                // Đối tượng Chức Vụ
+                String LKHName = row.getCell(3).getStringCellValue();
+
+//                nhanVien.set(row.getCell(4).getStringCellValue());
+                khachHang.setTrangThai(1);
+                khachHang.setTgThem(new Date());
+                khachHangRepository.save(khachHang);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println(e);
+            // Xử lý lỗi nếu cần
+        }
+    }
+
+
+    @Override
     public List<KhachHang> findByTrangThai(int trangThai) {
         return khachHangRepository.findByTrangThai(trangThai);
     }
 
     @Override
-    public List<KhachHang> fillterKhachHang(String maKh, String tenKh) {
-        if("Mã Khách Hàng".equals(maKh) && "Tên Khách Hàng".equals(tenKh)){
+    public List<KhachHang> fillterKhachHang(String maKH, String tenKH) {
+        if ("Mã Khách Hàng".equals(maKH) && "Tên Khách Hàng".equals(tenKH)) {
             return khachHangRepository.findAll();
         }
-        return khachHangRepository.findByMaKHOrHoTenKH(maKh, tenKh);
+        return khachHangRepository.findByMaKHOrHoTenKH(maKH, tenKH);
     }
 
     @Override
@@ -69,5 +111,6 @@ public class KhachHangServiceImpl  implements KhachHangService {
     @Override
     public List<KhachHang> findKhachHangByKeyword(String keyword) {
         return khachHangRepository.findByHoTenKHOrSdtKH(keyword);
+
     }
 }
