08594a7ecde26b546cc757d081a6292b63f99c55
thuybeo03 <thuynmph22235@fpt.edu.vn>
2024-06-15 20:07:30 +0700

    a

--- src/main/java/com/example/shoesmanagement/buyerController/CartController.java
+++ src/main/java/com/example/shoesmanagement/buyerController/CartController.java
@@ -152,7 +152,6 @@ public class CartController {
         if (!listGHCTActive.isEmpty()) {
             for (GioHangChiTiet gioHangChiTiet : listGHCTActive) {
                 gioHangChiTiet.setDonGia(gioHangChiTiet.getChiTietGiay().getGiaBan() * gioHangChiTiet.getSoLuong());
-                gioHangChiTiet.setDonGiaTruocKhiGiam(gioHangChiTiet.getChiTietGiay().getSoTienTruocKhiGiam() * gioHangChiTiet.getSoLuong());
                 ghctService.addNewGHCT(gioHangChiTiet);
             }
         }
